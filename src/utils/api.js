e2d514adcb147896aaa0e3de7cd72776e1733cc3
shahjahan71 <sm.shahjahan71.ss@gmail.com>
2025-06-28 17:36:52 +0600

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'alam-asia-paribahan-server.vercel.app/api';
 
 export const fetchSchedule = async (dateString) => {
   try {
